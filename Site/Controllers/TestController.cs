524ce12bc1c2e7b34a0f51c4f1ecd8db65c29ba8
Alina.Gryaznova <Alina.Gryaznova@sigmaukraine.com>
2015-07-19 00:02:43 +0300

    makes it all work by adding correct connectionString

--- Site/Controllers/TestController.cs
+++ Site/Controllers/TestController.cs
@@ -3,16 +3,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using Repository.DataServices;
+using Repository.Models;
 
 namespace Site.Controllers
 {
     [AllowAnonymous]
     [EnableCors(origins: "*", headers: "*", methods: "*")]
-    public class TestController
+    public class TestController : ApiController
     {
-        public async Task<IEnumerable<int>> Get()
+        private readonly IUserService service;
+
+        public TestController(IUserService service)
         {
-            return await Task.Factory.StartNew(() => Enumerable.Range(0,4).ToList());
+            this.service = service;
+        }
+
+        public async Task<User> Get()
+        {
+            return await Task.Factory.StartNew(() => service.GetUserByLogin("foo"));
         }
     }
 }
